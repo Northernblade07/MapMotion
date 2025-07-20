2e73551912192a102417d9f933712fa57f6c67d5
Prahant Bhandari <prashantbhandaricr07@gmail.com>
2025-07-20 09:57:27 +0530

    setup dummy data in constants

---
+++ src/constant/dummy-route.js
@@ -0,0 +1,8 @@
+export const dummyRoute = [
+  { latitude: 17.385044, longitude: 78.486671, timestamp: "2024-07-20T10:00:00Z" },
+  { latitude: 17.386000, longitude: 78.487000, timestamp: "2024-07-20T10:00:05Z" },
+  { latitude: 17.387500, longitude: 78.488000, timestamp: "2024-07-20T10:00:10Z" },
+  { latitude: 17.389000, longitude: 78.489000, timestamp: "2024-07-20T10:00:15Z" },
+  { latitude: 17.390500, longitude: 78.489500, timestamp: "2024-07-20T10:00:20Z" },
+  { latitude: 17.392000, longitude: 78.490000, timestamp: "2024-07-20T10:00:25Z" }
+];
